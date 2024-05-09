658d816cd91fc234ff80e9de58d7f7cb660b2acf
rXavier-L <ramonxavierleal@gmail.com>
2024-05-09 12:13:39 -0300

    Banco Xavier Commit projeto

---
+++ ContaBanco/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Contaterminal","l":"Contaterminal()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Contaterminal","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
--- ContaBanco/src/Contaterminal.java
+++ ContaBanco/src/Contaterminal.java
@@ -1,5 +1,57 @@
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Ramon Xavier Leal
+ * @version 1.0.0
+ * @category System de banco basic
+ * 
+ * @param nome  retorna nome do usuario
+ * @param pin   retorna codigo cadastrado
+ * @param valor retorna valor digitado pelo usuario
+ * @param sacar retora valor sacado
+ *              sistema de cadastro de banco basico com input de dinheiro e
+ *              saque.
+ */
 public class Contaterminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        String[] lines = {
+                "#####      ###    ##   ##   #####    #####            ##   ##    ###    ##   ##   ######  #######  ######",
+                "##   ##   ## ##   ###  ##  ##   ##  ##   ##           ##   ##   ## ##   ##   ##     ##    ##       ##   ##",
+                "##   ##  ##   ##  #### ##  ##       ##   ##            #####   ##   ##  ##   ##     ##    ##       ##   ##",
+                "######   ##   ##  ## ####  ##       ##   ##             ###    ##   ##  ##   ##     ##    #####    ##   ##",
+                "##   ##  #######  ##  ###  ##       ##   ##            #####   #######  ##   ##     ##    ##       ######",
+                "##   ##  ##   ##  ##   ##  ##   ##  ##   ##           ##   ##  ##   ##   ## ##      ##    ##       ##  ##",
+                "#####    ##   ##  ##   ##   #####    #####            ##   ##  ##   ##    ###     ######  #######  ##   ##"
+        };
+
+        String nome;
+        int pin = 0;
+        float valor = 0, sacar = 0;
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
+        
+        System.out.println("\ndigite seu nome:");
+        nome = scanner.next();
+
+        System.out.println("\ndigite seu codigo: ");
+        pin = scanner.nextInt();
+
+        System.out.println("\ndigite seu saldo: ");
+        valor = scanner.nextFloat();
+
+        System.out.println("bem vindo - " + nome);
+        System.out.println("seu saldo é de:" + valor);
+        System.out.println("Usuario numero " + pin + " de pin\n");
+        System.out.println("digite o valor que deseja sacar: ");
+        if (sacar > valor) {
+            System.out.println("\nvalor insuficiente para saque!\n");
+
+        } else System.out.println("\no valor atual após o saque é de: " + (valor - sacar));
     }
 }
